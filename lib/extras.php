52d6c1c32bb110b6848ec8ef7ba3d8f01e09054f
Ranno Ait <rannoait@gmail.com>
2017-04-17 13:56:24 +0300

    Added extras and wordpress setup extenstions
    * Open-Graph tags
    * Google Analytics
    * Favicons
    * Removed adminbar from website when loged in

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,90 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
+/**
+ * Open-Graph Tags
+*/
+function sage_add_opengraph_doctype( $output ) {
+    return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+  }
+add_filter('language_attributes', __NAMESPACE__ . '\\sage_add_opengraph_doctype');
+
+
+function sage_open_graph_tags() {
+  global $post;
+
+  $placeholder_image = get_stylesheet_directory_uri() . '/dist/images/placeholder.png';
+
+  if ( !is_singular()) {
+
+    echo '<meta property="og:type" content="article"/>';
+    echo '<meta property="og:site_name" content="' . get_bloginfo('name') . '"/>';
+    echo '<meta property="og:url" content="' . get_bloginfo('url') . '"/>';
+    echo '<meta property="og:title" content="' . get_bloginfo('name') . '"/>';
+    echo '<meta property="og:description" content="' . get_bloginfo('description') . '"/>';
+    echo '<meta property="og:image:width" content="1200"/>';
+    echo '<meta property="og:image:height" content="630"/>';
+    echo '<meta property="og:image" content="' . $placeholder_image . '"/>';
+    echo '<meta name="twitter:card" content="summary_large_image">';
+    echo '<meta name="twitter:title" content="' . get_bloginfo('name') . '">';
+    echo '<meta name="twitter:description" content="' . get_bloginfo('description')  . '">';
+    echo '<meta name="twitter:image" content="' . $placeholder_image . '">';
+    echo '<meta itemprop="name" content="' . get_bloginfo('name') . '"/>';
+    echo '<meta itemprop="description" content="' . get_bloginfo('description') . '"/>';
+    echo '<meta itemprop="image" content="' . $placeholder_image . '">';
+  
+  } else {
+
+    $content = $post->post_content;
+    $description = $content;
+    $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+    $og_image = ( has_post_thumbnail( $post->ID ) ) ? $featured_image[0] : $placeholder_image;
+
+    // echo '<meta property="fb:admins" content="YOUR USER ID"/>';
+    echo '<meta property="og:type" content="article"/>';
+    echo '<meta property="og:site_name" content="' . get_bloginfo('name') . '"/>';
+    echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+    echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+    echo '<meta property="og:description" content="' . strip_tags(substr($description, 0, 250)) . '"/>';
+    echo '<meta property="og:image:width" content="1200"/>';
+    echo '<meta property="og:image:height" content="630"/>';
+    echo '<meta property="og:image" content="' . $og_image . '"/>';
+    echo '<meta name="twitter:card" content="summary_large_image">';
+    // echo '<meta name="twitter:site" content="@soome100">';
+    // echo '<meta name="twitter:creator" content="@soome100">';
+    echo '<meta name="twitter:title" content="' . get_the_title() . '">';
+    echo '<meta name="twitter:description" content="' . strip_tags(substr($description, 0, 120)) . '">';
+    echo '<meta name="twitter:image" content="' . $og_image . '">';
+    echo '<meta itemprop="name" content="' . get_the_title() . '"/>';
+    echo '<meta itemprop="description" content="' . strip_tags(substr($description, 0, 250)) . '"/>';
+    echo '<meta itemprop="image" content="' . $og_image . '">';
+  }
+}
+add_action( 'wp_head', __NAMESPACE__ . '\\sage_open_graph_tags', 5 );
+
+
+/**
+ * Favicons
+*/
+function sage_favicons() {
+  echo '<link rel="apple-touch-icon" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/apple-touch-icon.png">';
+  echo '<link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/favicon-32x32.png" sizes="32x32">';
+  echo '<link rel="icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/favicon-16x16.png" sizes="16x16">';
+  echo '<link rel="manifest" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/manifest.json">';
+  echo '<link rel="mask-icon" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/safari-pinned-tab.svg" color="#002ea2">';
+  echo '<link rel="shortcut icon" href="' . get_stylesheet_directory_uri() . '/dist/images/favicons/favicon.ico">';
+  echo '<meta name="msapplication-config" content="/favicons/browserconfig.xml">';
+  echo '<meta name="theme-color" content="#ffffff">';
+}
+add_action( 'wp_head', __NAMESPACE__ . '\\sage_favicons', 6 );
+
+
+/**
+ * Google Analytics
+*/
+function sage_google_analytics() {
+  echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-CODE-HERE', 'auto'); ga('send', 'pageview');</script>";
+}
+add_action('wp_footer', __NAMESPACE__ . '\\sage_google_analytics');
--- lib/setup.php
+++ lib/setup.php
@@ -47,8 +47,82 @@ function setup() {
   // Use main stylesheet for visual editor
   // To add custom styles edit /assets/styles/layouts/_tinymce.scss
   add_editor_style(Assets\asset_path('styles/main.css'));
+
+  // Hide Admin bar when logged in
+  add_filter('show_admin_bar', '__return_false');
+
+  // Remove tags from <head>
+  remove_action('wp_head', 'rest_output_link_wp_head', 10);
+  remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+  remove_action('wp_head', 'wp_resource_hints', 2);
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'start_post_rel_link');
+  remove_action('wp_head', 'index_rel_link');
+  remove_action('wp_head', 'adjacent_posts_rel_link');
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'rel_canonical');
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+  // Disable emoji support
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
+
+/**
+* Remove Tabs from Admin page
+*/
+function remove_menu_pages() {
+  //remove_menu_page('tools.php');
+  remove_menu_page('edit-comments.php');
+  remove_menu_page('edit.php');
+  //remove_menu_page('plugins.php');
+}
+add_action( 'admin_menu', __NAMESPACE__ .  '\\remove_menu_pages' );
+
+
+/**
+* Reorder Admin Menu
+*/
+function custom_menu_order($menu_ord) {
+  if (!$menu_ord) return true;
+  return array(
+    'index.php', // Dashboard
+    // 'edit.php?post_type=custom_type_two', // Custom type two
+    // 'edit.php?post_type=custom_type_three', // Custom type three
+    // 'edit.php?post_type=custom_type_four', // Custom type four
+    // 'edit.php?post_type=custom_type_five', // Custom type five
+    'separator1', // First separator
+    'edit.php', // Posts
+    'edit.php?post_type=page', // Pages
+    'upload.php', // Media
+    'link-manager.php', // Links
+    'edit-comments.php', // Comments
+    'separator2', // Second separator
+    'themes.php', // Appearance
+    'plugins.php', // Plugins
+    'users.php', // Users
+    'tools.php', // Tools
+    'options-general.php', // Settings
+    'separator-last', // Last separator
+  );
+}
+
+add_filter('custom_menu_order', __NAMESPACE__ . '\\custom_menu_order');
+add_filter('menu_order', __NAMESPACE__ . '\\custom_menu_order');
 
 /**
  * Register sidebars
